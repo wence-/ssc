304ceb001384d420bdcf174f4258573caaab9451
Patrick Farrell <patrick@pefarrell.org>
2018-02-28 12:24:54 +0000

    OrderedStar, will clean up later

--- ssc/relaxation.py
+++ ssc/relaxation.py
@@ -1,7 +1,8 @@
 import numpy
+from itertools import chain
 from firedrake.petsc import PETSc
 
-__all__ = ['PlaneSmoother', 'OrderedVanka']
+__all__ = ['PlaneSmoother', 'OrderedVanka', 'OrderedStar']
 
 class PlaneSmoother(object):
     @staticmethod
@@ -120,6 +121,74 @@ class OrderedVanka(object):
         out = []
         for x in ec:
             subentities = self.vanka_callback(dm, x[0], nclosure)
+            iset = PETSc.IS().createGeneral(subentities, comm=PETSc.COMM_SELF)
+            out.append(iset)
+        return out
+
+class OrderedStar(object):
+    @staticmethod
+    def star_callback(dm, entity, nclosures):
+        out = set()
+        out.add(entity)
+
+        for c in range(nclosures):
+            new = set()
+            for entity in out:
+                star = dm.getTransitiveClosure(entity, useCone=False)[0]
+                for pt in star:
+                    new.add(pt)
+            out = out.union(new)
+        return list(out)
+
+    @staticmethod
+    def coords(dm, p):
+        coordsSection = dm.getCoordinateSection()
+        coordsDM = dm.getCoordinateDM()
+        dim = coordsDM.getDimension()
+        coordsVec = dm.getCoordinatesLocal()
+        return dm.getVecClosure(coordsSection, coordsVec, p).reshape(-1, dim).mean(axis=0)
+
+    def __call__(self, pc):
+        dm = pc.getDM()
+        prefix = pc.getOptionsPrefix()
+        sentinel = object()
+        opts = PETSc.Options(prefix)
+
+        codim = opts.getInt("pc_patch_construction_ostar_codim", default=sentinel)
+        if codim == sentinel:
+            dim = opts.getInt("pc_patch_construction_ostar_dim", default=0)
+            entities = range(*dm.getDepthStratum(dim))
+        else:
+            entities = range(*dm.getHeightStratum(dim))
+
+        nclosure = opts.getInt("pc_patch_construction_ostar_nclosures", default=1)
+
+        ec = ((p, self.coords(dm, p)) for p in entities)
+
+        sortorders = opts.getString("pc_patch_construction_ostar_sort_order", default=sentinel)
+        if sortorders == sentinel:
+            raise ValueError("Must set %spc_patch_construction_ostar_sort_order" % prefix)
+        if sortorders != "None":
+            outecs = []
+            for sortorder in sortorders.split("|"):
+                sortdata = []
+                for axis in sortorder.split(':'):
+                    ax = int(axis[0])
+                    if len(axis) > 1:
+                        sgn = {'+': 1, '-': -1}[axis[1]]
+                    else:
+                        sgn = 1
+                    sortdata.append((ax, sgn))
+
+                def keyfunc(z):
+                    return tuple(sgn*z[1][ax] for (ax, sgn) in sortdata)
+
+                outecs = chain(outecs, sorted(ec, key=keyfunc))
+            ec = outecs
+
+        out = []
+        for x in ec:
+            subentities = self.star_callback(dm, x[0], nclosure)
             iset = PETSc.IS().createGeneral(subentities, comm=PETSc.COMM_SELF)
             out.append(iset)
         return out
